312a783c0d240d8f36d5e8ec926d5dc0793c8284
Auridebson Sousa <auridebson@vutus.com>
2023-08-29 05:26:30 -0300

    Update

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const editForm = document.querySelector('#edit-form')
 const editInput = document.querySelector('#edit-input')
 const cancelEditBtn = document.querySelector('#cancel-edit-btn')
 
-
-
+let oldInputValue
 
 
 
@@ -41,9 +40,24 @@ const saveTodo = (text) => {
     todoInput.focus()
 }
 
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
 
+const updateTodo = (text) => {
+    const todos =  document.querySelectorAll("todo")
 
+    todos.forEach(todo) => {
+        let todoTitle = todo.querySelector("h3")
 
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    }
+
+}
 
 
 // Eventos
@@ -57,3 +71,46 @@ todoForm.addEventListener("submit", (e) => {
     }
 })
 
+document.addEventListener("click",(e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div")
+    let todoTitle
+
+    if (parentEl && parentEl.querySelector("div")) {
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
+
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")
+
+    }
+
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove()
+    }
+
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue.value = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const editInputValue =  editInput.value
+
+    if (editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
+})
